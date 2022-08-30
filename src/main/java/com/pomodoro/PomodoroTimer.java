3a414eed4d7b559139abc13d889ff6871a50eae6
Sergei Kolisni4enko <skolisni4enko@gmail.com>
2022-08-30 16:34:06 +0300

    first commit

--- src/main/java/com/pomodoro/PomodoroTimer.java
+++ src/main/java/com/pomodoro/PomodoroTimer.java
@@ -2,6 +2,7 @@ package com.pomodoro;
 
 public class PomodoroTimer {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello world");
+        System.out.println("");
     }
 }
