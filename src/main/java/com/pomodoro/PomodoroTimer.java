bbe099e82a6380eb29f1316e27ff9a5d9948ec3c
Sergei Kolisni4enko <skolisni4enko@gmail.com>
2022-09-01 13:27:17 +0300

    Timer Done!

--- src/main/java/com/pomodoro/PomodoroTimer.java
+++ src/main/java/com/pomodoro/PomodoroTimer.java
@@ -5,22 +5,28 @@ import java.util.concurrent.TimeUnit;
 
 public class PomodoroTimer {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("Введите время работы и отдыха:");
+        System.out.println("Введите время работы, время отдыха, количество циклов, множитель времени работы:");
         var cmd = new Scanner(System.in).nextLine().split(" ");
 
         int workTime = 1;
         int breakTime=1;
+        int iteration = 1;
+        int mult = 1;
         boolean isHelp = false;
 
         for (int i = 0; i < cmd.length; i++) {
             switch (cmd[i]){
                 case "-w" -> workTime = Integer.parseInt(cmd[++i]);
                 case "-b" -> breakTime = Integer.parseInt(cmd[++i]);
+                case "-count" -> iteration = Integer.parseInt(cmd[++i]);
+                case "-m" -> mult = Integer.parseInt(cmd[++i]);
                 case "--help" -> {
                     System.out.println("""
                         \nPomodoro - это приложение для улучшения личной эффективности
                         -w - сколько работать
                         -b - сколько отдыхать
+                        -count - количество циклов
+                        -m - множитель для времени работы
                         --help - вызвать помощь
                         """);
                     isHelp = true;
@@ -29,17 +35,19 @@ public class PomodoroTimer {
         }
         if (isHelp) return;
         long startTimer = System.currentTimeMillis();
-        timer(workTime, breakTime);
+        progression(workTime, breakTime, iteration, mult);
         long endTime = System.currentTimeMillis();
 
         System.out.println("Таймер работал " + (endTime-startTimer)/(1000*60) + " min.");
     }
 
+    /**********************************************************************/
     public static void timer(int workTime, int breakTime) throws InterruptedException {
         printProgress("Work progress bar ", workTime, 30);
         printProgress("Break progress bar ", breakTime, 30);
     }
 
+    /**********************************************************************/
     private static void printProgress(String process, int time, int size) throws InterruptedException {
         int length;
         int rep;
@@ -64,6 +72,14 @@ public class PomodoroTimer {
             if (true) {
                 TimeUnit.SECONDS.sleep(length);
             }
+        }
+    }
+/**********************************************************************/
+    private static void progression(int workTime, int breakTime, int iteration, int mult) throws InterruptedException {
+        timer(workTime, breakTime);
+        for (int i = 2; i <= iteration; i++) {
+            timer(workTime *= mult, breakTime);
+
         }
     }
 }
